73555beb9236bd37a16686b32c353801bb1c0a8a
pockocmoc <pockocmoc@inbox.ru>
2022-08-28 16:06:46 +0300

    Нахождение второго по максимальности элемента массива.

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,20 +1,28 @@
+// Нахождение второго по максимальности элемента массива.
+
 public class Algorithm {
     public static void main(String[] args) {
 
-        int[] mas = {1, 8, 3, 2, 6};
-        int maxNumber = 0;
-        int maxIndex = 0;
+        int[] intArray = {1, 8, 3, 2, 6, 4, 10};
+        int maxNum = 0;
+        int maxSecondNum = 0;
 
-        for (int i = 0; i < mas.length; i++) {
-            if(maxNumber <= mas[i]){
-                maxNumber = mas[i];
-                maxIndex = i;
+
+        for (int i = 0; i < intArray.length; i++) {
+            if (intArray[i] > maxNum) {
+                maxSecondNum = maxNum;
+                maxNum = intArray[i];
+
+            } else if (intArray[i] > maxSecondNum && intArray[i] != maxNum) {
+                maxSecondNum = intArray[i];
+
             }
         }
+        if (maxSecondNum != Integer.MAX_VALUE) {
+            System.out.println("Второй по максимальности элемент массива: " + maxSecondNum);
+        } else {
+            System.out.println("Второй по максимальности элемент массива отсутствует. ");
 
-        System.out.println("Индекс максимального элемента: " + maxIndex);
+        }
     }
 }
-
-
-
